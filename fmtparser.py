499a9ce3f9fcbaad591758082446436d3311239f
derlinkshaender <mail@arminhanisch.de>
2013-07-30 00:27:25 +0200

    fmtparser stub

---
+++ fmtparser.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*.
+
+import sys
+import os
+
+
+TEST = '127.0.0.1 user-identifier frank [10/Oct/2000:13:56:08 -0700] "GET /apache_pb.zap HTTP/1.0" 404 0'
+
+print TEST
+
